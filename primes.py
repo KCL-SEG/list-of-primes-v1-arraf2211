53054937c1e845f245f999f3c6dd6dcfb45f358a
arraf2211 <arrafrahman22@gmail.com>
2022-10-02 21:54:27 +0100

    add neccessary functions

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    x = 3
+    if number_of_primes > 0:
+        list.append(2)
+        while len(list) < number_of_primes:
+            flag = False
+            for i in range(2, x):
+                if x % i == 0:
+                    flag = False
+                    break
+                else:
+                    flag =True
+            if flag:
+                list.append(x)
+            x = x + 1
+
+
     return list
